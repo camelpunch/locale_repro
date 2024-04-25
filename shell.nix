d5aac806c2efebbcd22e6619a01aa1f417c1bd7d
Andrew Bruce <me@andrewbruce.net>
2024-04-25 09:15:04 +0100

    elixir_ls

--- shell.nix
+++ shell.nix
@@ -1,9 +1,19 @@
-{ mkShell
-, elixir_1_16
-}:
+{ pkgs }:
+
+with pkgs;
 
 mkShell {
   packages = [
     elixir_1_16
+    ((pkgs.elixir_ls.override {
+      elixir = elixir_1_16;
+    }).overrideAttrs
+      {
+        buildPhase = ''
+          runHook preBuild
+          mix do compile --no-deps-check, elixir_ls.release2
+          runHook postBuild
+        '';
+      })
   ];
 }
